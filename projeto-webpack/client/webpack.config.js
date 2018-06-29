ac4e7fc43bd14eb7eb0ce1b548e13068b4d84ff8
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-06-29 17:27:40 -0300

    Using module in frontend

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -25,7 +25,26 @@ module.exports = {
                 use: {
                     loader: 'babel-loader' // vai acessar o babelrc e aplicar babel-core nos arquivos
                 }
-
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader', // direita para esquerda
+            },
+            { 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
             }
         ]
     },
