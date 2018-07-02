c632e1bb74fe33f3baeaf782458089d1007f4cba
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-07-02 11:04:54 -0300

    Imporing scripts and minify css

--- projeto-webpack/client/app-src/app.js
+++ projeto-webpack/client/app-src/app.js
@@ -2,6 +2,7 @@ import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/dist/js/bootstrap.js'
 import '../css/style.css';
 
 const controller = new NegociacaoController();
--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -1,10 +1,31 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const webpack = require('webpack');
 
 let plugins = [];
 
+plugins.push(new extractTextPlugin('style.css'));
+
+// dependencia global, disponibilidade de artefatos para para outros modulos
+plugins.push(new webpack.ProvidePlugin({
+    '$': 'jquery/dist/jquery.js',
+    'jQuery': 'jquery/dist/jquery.js'
+}));
+
 if (process.env.NODE_ENV == 'production') {
     plugins.push(new babiliPlugin());
+    plugins.push(new optimizeCSSAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: {
+            discardComments: {
+                removeAll: true
+            }
+        },
+        canPrint: true
+    }));
 }
 
 module.exports = {
@@ -28,7 +49,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader', // direita para esquerda
+                // loader: 'style-loader!css-loader', // direita para esquerda
+                use: extractTextPlugin.extract({
+                    fallback: 'style-loader', // caso falhe
+                    use: 'css-loader' // padrao para uso
+                })
             },
             { 
                 test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
